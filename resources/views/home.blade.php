cead8992fb9782076b1d2b8cd3b4a4561fd9a95b
alessandro-barate <alebarat@gmail.com>
2024-07-04 14:37:31 +0100

    [UPD] Typo corrected

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LAravel Hello World!</title>
+    <title>Laravel Hello World!</title>
 </head>
 
 <body>
